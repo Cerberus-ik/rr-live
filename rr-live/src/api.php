bb116c91103fa059fb47c763be2d8259e9e1eb62
Gerolf Vent <gerolf.vent@gmx.de>
2017-12-31 16:20:05 +0100

    Added central api url variable Added APIs getAnalyzedRunes and getMostPickedRune Some minor changes

--- rr-live/src/api.php
+++ rr-live/src/api.php
@@ -1,13 +1,16 @@
 <?php
+$riotBackendApi = "https://riot-backend-api.herokuapp.com/api/v1";
 if ($_GET["api"] == "runes") {
-	echo file_get_contents("https://riot-backend-api.herokuapp.com/api/v1/runes/getRunes?id=".$_GET["id"]."&amount=".$_GET["amount"]);
+	echo file_get_contents($riotBackendApi."/runes/getRunes?id=".$_GET["id"]."&amount=".$_GET["amount"]);
 } else if ($_GET["api"] == "descriptions") {
 	echo file_get_contents("http://ddragon.leagueoflegends.com/cdn/7.24.1/data/en_US/runesReforged.json");
 } else if ($_GET["api"] == "patches") {
-	echo file_get_contents("https://riot-backend-api.herokuapp.com/api/v1/patch/getPatches?since=".$_GET["since"]);
+	echo file_get_contents($riotBackendApi."/patch/getPatches?since=".$_GET["since"]);
 } else if ($_GET["api"] == "maxpossibleid") {
-	echo file_get_contents("https://riot-backend-api.herokuapp.com/api/v1/runes/getLatestStepId");
-} else if ($_GET["api"] == "mostusedrune") {
-	echo file_get_contents("https://riot-backend-api.herokuapp.com/api/v1/fact/getMostDominantRune");
+	echo file_get_contents($riotBackendApi."/runes/getLatestStepId");
+} else if ($_GET["api"] == "analysedrunes") {
+	echo file_get_contents($riotBackendApi."/fact/getAnalyzedRunes");
+} else if ($_GET["api"] == "mostpickedrolerunes") {
+	echo file_get_contents($riotBackendApi."/fact/getMostPickedRunes");
 }
 ?>
